6c61f891648bb8db9e676515d90de5fe2ed0b9ad
heffoo <57847291+heffoo@users.noreply.github.com>
2020-10-30 18:06:32 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather/precache-manifest.a870042f25cc26563533169afada38da.js"
+  "/weather/precache-manifest.fb88b69ee8afc0ca22ce33bb3bed1402.js"
 );
 
 self.addEventListener('message', (event) => {
